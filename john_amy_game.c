9693505d75ef864ec04d777e72a40c9fee5f3ed8
inwoo-school <oh6816@hanyang.ac.kr>
2024-07-11 11:48:05 +0900

    commit

---
+++ john_amy_game.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int N;
+    int a, b;
+
+    printf("type a number (range: 1 <= N <= 1000)\n");
+    scanf("%d", &N);
+
+    if (!(N >= 1 && N <= 1000))
+    {
+        printf("input of range\n");
+        exit(-1);
+    }
+
+    for (a = 1; a <= 500; a++)
+    {
+        for (b = 1; b <= a; b++)
+        {
+            if ((a*a - b*b) == N)
+            {
+                printf("(%d,%d)\n",a,b);
+            }
+        }
+    }
+}
+
+
